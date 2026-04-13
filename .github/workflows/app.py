8abf0d63b715029ad4a6961ad5be686d3a9de4c9
Your Name <you@example.com>
2026-04-13 23:04:15 +0530

    added

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
@@ -12,7 +11,3 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
-
-
-if __name__ == "__main__":
-    app.run(port=80)
