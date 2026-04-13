458d97f489787f412ac4660f3211090fc7c4b107
Your Name <you@example.com>
2026-04-13 22:44:33 +0530

    added

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,16 +1,18 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-flake8
-flask==3.1.3
-bandit
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html')
+from flask import Flask, render_template
 
 
+app = Flask(__name__)
 
-# @app.route('/health')
-# def health():
-#     return 'Server is up and running'
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
+
+
+if __name__ == "__main__":
+    app.run(port=80)
--- app.py
+++ app.py
@@ -9,10 +9,7 @@ def hello_world():
     return render_template('index.html')
 
 
+
 @app.route('/health')
 def health():
-    return 'Server is up and running'
-
-
-if __name__ == "__main__":
-    app.run(port=80)
+   return 'Server is up and running'
