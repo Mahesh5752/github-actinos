3d96bf91cdbb7d827ce34006be5cc8eb1504f116
Your Name <you@example.com>
2026-04-16 23:45:09 +0530

    Fix spacing and sync with remote

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
@@ -13,5 +12,4 @@ def health():
     return 'Server is up and running'
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+app.run(port=80)
